aef32d9d4a012ab2a3e70b8ddf8ed79971e4d113
excalord <hyukaruu@icloud.com>
2025-05-10 21:13:53 -0500

    Games

---
+++ number_guess.py
@@ -0,0 +1,40 @@
+import random
+
+max_number = input(
+    "Type the max of the numbers that the machine will use for the guess game"
+)
+
+if max_number.isdigit():
+    max_number = int(max_number)
+
+    if max_number <= 0:
+        print("Please digit a number higher than 0 next time: ")
+        quit()
+else:
+    print("Please digit a number next time")
+    quit()
+
+
+random_number = random.randint(0, max_number)
+guess_attempts = 0
+
+while True:
+    guess_attempts += 1
+    user_input = input("Digit the number that the machine is thinking! ")
+
+    if user_input.isdigit():
+        user_input = int(user_input)
+    else:
+        print("Please type a number, the last one you digit is not a number")
+        continue
+
+    if user_input == random_number:
+        print("You have guessed the number")
+        print(f"Took to you {guess_attempts} attempts to guess the number")
+        break
+    elif user_input < random_number:
+        print("You are lower than the number")
+    else:
+        print("You are upper than the number")
+
+print("The game have end!")
---
+++ quiz_game.py
@@ -0,0 +1,46 @@
+print("Do you want to play the quiz? ")
+
+playing = input("Yes or No? ").lower()
+if playing != "yes":
+    quit()
+
+print("Okay! Let's Play :)")
+
+score = 0
+
+answer = input("What does the CPU stand for? ").lower()
+if answer == "central processing unit":
+    print("Correct!!")
+    score += 1
+else:
+    print("Incorrect!!")
+
+answer = input("What does the GPU stand for? ").lower()
+if answer == "graphics processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!!")
+
+answer = input("What does the RAM stand for? ").lower()
+if answer == "random access memory":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!!")
+
+answer = input("What does the VRAM stand for? ").lower()
+if answer == "virtual random access memory":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!!")
+
+answer = input("What does the OS stand for? ").lower()
+if answer == "operating system":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!!")
+
+print("You got " + str(score) + " questions correct!")
